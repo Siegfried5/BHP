74348b4e7866db09f980f8acc62ba0484ba943b3
Siegfried5 <rudolph.ponce745@gmail.com>
2015-10-06 07:52:32 -0700

    finished Netcat.py

--- Chapter 2/Netcat.py
+++ Chapter 2/Netcat.py
@@ -80,4 +80,43 @@ def main ():
 	# depending on our commmand line options above
 	if listen:
 		server_loop()
+
+def client_sender(buffer):
+
+	client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+	try:
+		# connect to our target host
+		client.connect((target,port))
+		if len(buffer):
+			client.send(buffer)
+		while True:
+
+			#now wait for data back
+			recv_len = 1
+			response=""
+
+			while recv_len:
+
+				data = client.recv(4096)
+				recv_len = len(data)
+				response+= data
+
+				if recv_len < 4096:
+					break
+
+			print response,
+
+			# wait for more input
+			buffer = raw_input("")
+			buffer+= "\n"
+
+			# send it off
+			client.send(buffer)
+	except:
+		print "[+] Exception! Exiting."
+
+		# tear down the connection
+		client.close()
+
 main()
