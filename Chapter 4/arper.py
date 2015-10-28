d23125fcf4a11f50dfbc859186839cf2d2df3b6b
Siegfried5 <rudolph.ponce745@gmail.com>
2015-10-27 19:12:12 -0700

    debuged arper.py

--- Chapter 4/arper.py
+++ Chapter 4/arper.py
@@ -14,7 +14,7 @@ def restore_target(gateway_ip,gateway_mac,target_ip,target_mac):
 	# slightly different method using send
 	print "[+] Restoring target..."
 	send(ARP(op=2, pstc=gateway_ip, pdst=target_ip, hwdst="ff:ff:ff:ff:ff:ff",hwsrc=gateway_mac),count=5)
-	send(ARP(op=2, psrc=target_ip, psrc=gateway_ip, hwdst="ff:ff:ff:ff:ff:ff",hwsrc=target_mac),count=5)
+	send(ARP(op=2, psrc=target_ip, pdst=gateway_ip, hwdst="ff:ff:ff:ff:ff:ff",hwsrc=target_mac),count=5)
 	# signals the main thread to  exit
 	os.kill(os.getpid(), signal.SIGINT)
 
