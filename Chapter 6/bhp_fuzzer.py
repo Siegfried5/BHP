dd6c2647046fba0355368d6fc03a727d5e304a74
Siegfried5 <rudolph.ponce745@gmail.com>
2015-11-11 09:56:18 -0700

    Changed the name of Black hat python to Python Scripts

--- Chapter 6/bhp_fuzzer.py
+++ Chapter 6/bhp_fuzzer.py
@@ -19,3 +19,42 @@ class BurpExtender(IBurpExtender, IIntruderPayloadGeneratorFactory):
 	
 	def createNewInstance(self, attack):
 		return BHPFuzzer(self,attack)
+
+class BHPFuzzer (IIntruderPayloadGenerator):
+	def __init__(self, extender, attack):
+		self._extender = extender
+		self._helpers = extender._helpers
+		self._attack = attack
+		self.max_payloads = 10
+		self.num_iterations = 0
+
+		return
+
+	def hasMorePayloads(self):
+		if self.num_iterations == self.max_payloads:
+			return False
+		else:
+			return True
+
+	def getNextPayload(self,current_payload):
+		
+		# convert into a string
+		payload = "".join(chr(x) for x in current_payload)
+		# call our simple mutator to fuzz the POST
+		payload = self.mutate_payload(payload)
+
+		# incress the number of fuzzing attempts
+		self.num_iterations += 1
+
+		return payload
+
+	def reset(self):
+		self.num_iterations =  0
+		return
+
+	def mutate_payload(self,original_payload):
+		# pick a simple mutator or even call external script
+		picker = random.randint(1,3)
+
+		# select a random offset in the payload to mutate
+		offset = random.randint(0, len(original_payload)-1)
