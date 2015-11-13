1422582e1ca6a3beaaf8a7226cb082a651f88864
Siegfried5 <rudolph.ponce745@gmail.com>
2015-11-12 20:21:12 -0700

    started bhp_bing.py

---
+++ Chapter 6/bhp_bing.py
@@ -0,0 +1,12 @@
+from burp import IBurpExtender
+from burp import IContextMenuFactory
+from javax.swing import JMenuItem
+from java.util import List, ArrayList
+from java.net import URL
+import socket
+import urllib
+import json
+import re
+import base64
+
+bing_api_key ="YOUREKEY"
