751ca2c6ecb8d52dc7d7e02ad256333e0bf55c26
Siegfried5 <rudolph.ponce745@gmail.com>
2016-02-12 19:34:15 -0700

    finished process_monitor.py

--- Chapter 10/process_monitor.py
+++ Chapter 10/process_monitor.py
@@ -8,6 +8,29 @@ import wmi
 import sys
 import os
 
+def get_process_privileges(pid):
+	try:
+		# obtain a handle to the target process
+		hproc = win32api.OpenProcess(win32con.PROCESS_QUERY_INFORMATION,False,pid)
+		
+		# open the main process token
+		htok = win32security.OpenProcessToken(hproc,win32con.TOKEN_QUERY)
+		
+		# retrieve the list of privileges enabled
+		privs = win32security.GetTokenInformation(htok, win32security.TokenPrivileges)
+		
+		# iterate over privileges and output the ones that are enabled
+		priv_list = ""
+		for i in privs:
+			# check if the privilege is enabled
+			if i[1] == 3:
+				priv_list += "%s|" % win32security.LookipPrivilegeName(None,i[0])
+	
+	except:
+		priv_list = "N/A"
+	
+	return priv_list
+
 def log_to_file(message):
 	fd = open("process_monitor_log.cvs","ab")
 	fd.write("%s\r\n" % message)
